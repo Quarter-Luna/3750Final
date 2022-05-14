e17707ba6ff18c939d2f87f86343979b98fbe1c6
Ian Moon <55413858+Quarter-Luna@users.noreply.github.com>
2022-05-13 22:16:42 -0600

    some

--- mmult.c
+++ mmult.c
@@ -66,7 +66,7 @@ int main(int argc,char *argv[])
             fin[i][j] = 0;
             for(int k = 0; k < col2; k++)
             {
-                fin[i][j] += mat1[i][k] * mat2[k][j];
+                fin[i][j] += (mat1[i][k] * mat2[k][j]);
             }
         }
     }
