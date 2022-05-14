73af286cbc51a1a0f32de735f9e65f63a148ede2
Ian Moon <55413858+Quarter-Luna@users.noreply.github.com>
2022-05-13 20:35:58 -0600

    something

--- mmult.c
+++ mmult.c
@@ -16,6 +16,11 @@
 #include<pthread.h>
 #include<unistd.h>
 
+int readMatrix(FILE file)
+{
+    
+}
+
 int main(int argc, char *argv[])
 {
     // argument check
@@ -25,14 +30,32 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    char *mtx1 = argv[1], // matrix 1
-    *mtx2 = argv[2], // matrix 2
-    *out = argv[3]; // output file
+    FILE *mtx1 = fopen(argv[1], "r"), // matrix 1 file
+    *mtx2 = fopen(argv[2], "r"), // matrix 2 file
+    *out = fopen(argv[3], "w"); // output file
+    int threads;
 
     if(argc == 4)
     {
-        int threds = atoi(argv[4]);
+        threads = atoi(argv[4]);
     }
 
+    if(threads == NULL)
+    {
+        threads = 3;
+    }
 
+    pthread_t tid0, tid1, tid2, tid3, tid4, tid5, tid6, tid7, tid8, tid9; 
+
+    pthread_t * pthreads[] = {&tid0, &tid1, &tid2, &tid3, &tid4, &tid5, &tid6, &tid7, &tid8, &tid9};
+
+    int rix1[10][10], rix2[10][10];
+
+    for(int i = 0; i < threads; i++)
+    {
+        if(i == 1)
+        {
+            pthread_create(pthreads[i],NULL, readMatrix(*mtx1), (void *) pthreads[i]);
+        }
+    }
 }
